3ebd5f88743173e76f7b8444145d3a4b2b6b8e34
Sandeep Pareek <psandeep01@gmail.com>
2022-05-11 15:06:38 +0530

    Registry service

---
+++ src/test/java/com/sandeep/registryservice/RegistrySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrySerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
